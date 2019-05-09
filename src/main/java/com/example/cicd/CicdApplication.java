7e5334e0c6aea0c0dd58d642f1e171d31890176d
L1692 <franchiluigi92@yahoo.it>
2019-05-09 15:09:04 +0200

    Added CodeDeploy

--- src/main/java/com/example/cicd/CicdApplication.java
+++ src/main/java/com/example/cicd/CicdApplication.java
@@ -3,12 +3,17 @@ package com.example.cicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class CicdApplication {
 
+    private static final Logger logger = Logger.getLogger( CicdApplication.class.getName() );
+
     public static void main(String[] args) {
         SpringApplication.run(CicdApplication.class, args);
-        System.out.println("Hello World!");
+        logger.log(Level.FINE, "Hello World!");
     }
 
 }
--- src/test/java/com/example/cicd/CicdApplicationTests.java
+++ src/test/java/com/example/cicd/CicdApplicationTests.java
@@ -5,22 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.junit.Assert.assertEquals;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CicdApplicationTests {
 
     @Test
-    public void testConcatenate() {
-        MyUnit myUnit = new MyUnit();
-
-        String result = myUnit.concatenate("one", "two");
-
-        assertEquals("onetwo", result);
+    public void contextLoads() {
     }
 
 }
