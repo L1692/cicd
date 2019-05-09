bc0a09be95b794c4e8b1522880e3007c448c4769
L1692 <franchiluigi92@yahoo.it>
2019-05-09 17:54:04 +0200

    Added CodeDeploy

--- codedeploy/ApplicationStop.sh
+++ codedeploy/ApplicationStop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sudo docker ps -a
-sudo docker rmi 195360077735.dkr.ecr.eu-west-1.amazonaws.com/cicdapp:latest
+sudo docker rmi -f 195360077735.dkr.ecr.eu-west-1.amazonaws.com/cicdapp:latest
