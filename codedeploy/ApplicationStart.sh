8a74a99c5e10d9a68519af448024f333c62bb7b3
L1692 <franchiluigi92@yahoo.it>
2019-05-10 13:04:02 +0200

    Added CodeDeploy

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo $(aws ecr get-login --region eu-west-1 --no-include-email)
-sudo docker run -d -p 8080:8080 195360077735.dkr.ecr.eu-west-1.amazonaws.com/cicdapp:latest
+sudo docker run -d -p 8080:8000 195360077735.dkr.ecr.eu-west-1.amazonaws.com/cicdapp:latest
